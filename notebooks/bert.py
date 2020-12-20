8a2169bdfa3b96aa6edfa28329b8baa7df6521c4
Devansh Kaushik <45105530+devi777@users.noreply.github.com>
2020-12-20 20:07:32 +0530

    Add files via upload

---
+++ notebooks/bert.py
@@ -0,0 +1,25 @@
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+from official.modeling import tf_utils
+from official import nlp
+from official.nlp import bert
+
+# Load the required submodules
+import official.nlp.optimization
+import official.nlp.bert.bert_models
+import official.nlp.bert.configs
+import official.nlp.bert.run_classifier
+import official.nlp.bert.tokenization
+import official.nlp.data.classifier_data_lib
+import official.nlp.modeling.losses
+import official.nlp.modeling.models
+import official.nlp.modeling.networks
